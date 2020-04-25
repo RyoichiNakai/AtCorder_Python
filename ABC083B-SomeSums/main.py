41630be2ce951abaedf57a72bc68951d25d82f29
your git username <your git email>
2020-04-25 13:25:55 +0900

    new commit

--- ABC083B-SomeSums/main.py
+++ ABC083B-SomeSums/main.py
@@ -8,4 +8,7 @@ for i in range(1, N + 1):
     if A <= w & w <= B:
         sub += i
 
+# 普通　実行時間:37ms 表記のバイト数:206byte
 print(sub)
+# リスト内包表記　実行時間:46ms 表記のバイト数:146byte
+print(sum(i for i in range(1, N + 1) if A <= sum(list(map(int, str(i)))) & sum(list(map(int, str(i)))) <= B))
