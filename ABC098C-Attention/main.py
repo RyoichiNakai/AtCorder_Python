9a83fb1acf53855a8dcaf87aff089cfc1c2d2ec1
RyoichiNakai <elmobasketball0913@me.com>
2020-05-10 22:49:29 +0900

    new commit

--- ABC098C-Attention/main.py
+++ ABC098C-Attention/main.py
@@ -13,3 +13,30 @@ for i in range(n):
     ans = min(cnt, ans)
 
 print(ans)
+
+w_e = [0] * n
+e_w = [0] * n
+ans = []
+
+# 別解
+# 5
+# WEEWW
+# [0, 1, 1, 1, 2] [2, 1, 0, 0, 0]
+for i in range(n - 1):
+    # リーダより西側にいる人のなかで西に向いている人が東側に向くようにする(以下、反対向きの人と略)
+    # 累積和の考え方を用いて今のリーダの反対向きの人の数は前のリーダのそれに＋１してあげればいい
+    if s[i] == 'W':
+        w_e[i + 1] = w_e[i] + 1
+    else:
+        w_e[i + 1] = w_e[i] + 0
+
+for i in range(n - 2, -1, -1):  # 右側は開区間と覚えておく！！
+    if s[i + 1] == 'E':
+        e_w[i] = e_w[i + 1] + 1
+    else:
+        e_w[i] = e_w[i + 1] + 0
+
+for i in range(n):
+    ans.append(w_e[i] + e_w[i])
+
+print(min(ans))
