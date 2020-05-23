ab02db9bfa1b6b781ac0c739804a86097a02cbef
RyoichiNakai <elmobasketball0913@me.com>
2020-05-23 12:17:53 +0900

    new commit

--- ABC031B-Landfill/main.py
+++ ABC031B-Landfill/main.py
@@ -1,33 +1,18 @@
 import sys
+import copy
 
 sys.setrecursionlimit(10 ** 8)  # 再帰関数の呼び出し制限
 c = [list(input()) for i in range(10)]
+c_copy = copy.deepcopy(c)
 
 
-# todo: 陸と海を逆にして考えてみる(line10 'x' => 'o', line41)
+# つながっているところだけを探索する！
 def dfs(x, y):
-    flag = False
-    if not (0 <= x < 10) or not (0 <= y < 10) or c[x][y] == 'x' or c[x][y] == '#':  # 壁に当たったり、探索範囲外になった場合はreturn
+    if not (0 <= x < 10) or not (0 <= y < 10) or c_copy[x][y] == 'x':
+        # 壁に当たったり、探索範囲外になった場合はreturn
         return
 
-    c[x][y] = "#"  # 探索済みを示すためのマーキング
-    dx_dy_1 = [[1, 0], [-1, 0], [0, 1], [0, -1]]
-    dx_dy_2 = [[2, 0], [-2, 0], [0, 2], [0, -2]]
-
-    mass_1 = ''
-    mass_2 = ''
-    for i in range(4):
-        if 0 <= x + dx_dy_2[i][0] < 10 and 0 <= y + dx_dy_2[i][1] < 10:
-            mass_1 = c[x + dx_dy_1[i][0]][y + dx_dy_1[i][1]]
-            mass_2 = c[x + dx_dy_2[i][0]][y + dx_dy_2[i][1]]
-
-        if mass_1 == 'x' and mass_2 == 'o':
-            c[x + dx_dy_1[i][0]][y + dx_dy_1[i][1]] = '#'
-            flag = True
-
-    if flag:
-        print('YES')
-        sys.exit()
+    c_copy[x][y] = "x"  # 探索済みの島のところを海にする
 
     dfs(x + 1, y)  # これだとxがした方向、yが右方向
     dfs(x - 1, y)
@@ -35,17 +20,76 @@ def dfs(x, y):
     dfs(x, y - 1)
 
 
-cnt = 0
+def checked():
+    # 島が1つなればdfsで全体が海になる
+    for i in range(10):
+        for j in range(10):
+            if c_copy[i][j] == 'o':
+                return False
+    return True
+
+
 for i in range(10):
     for j in range(10):
-        if c[i][j] == 'o':
-            cnt += 1
+        if c[i][j] == 'x':
+            c_copy = copy.deepcopy(c)
+            c_copy[i][j] = 'o'
             dfs(i, j)
-
-            break
-
-if cnt == 1:
-    print('YES')
-    sys.exit()
+            if checked():
+                print('YES')
+                exit(0)
 
 print('NO')
+
+
+# def dfs_o(x, y):
+#     if not (0 <= x < 10) or not (0 <= y < 10) or c_o[x][y] == 'x' or c_o[x][y] == '#':  # 壁に当たったり、探索範囲外になった場合はreturn
+#         return
+#
+#     c_o[x][y] = "#"  # 探索済みを示すためのマーキング
+#
+#     dfs_o(x + 1, y)  # これだとxがした方向、yが右方向
+#     dfs_o(x - 1, y)
+#     dfs_o(x, y + 1)
+#     dfs_o(x, y - 1)
+#
+#
+# def dfs_x(x, y):
+#     if not (0 <= x < 10) or not (0 <= y < 10) or c_x[x][y] == 'o' or c_x[x][y] == '#':  # 壁に当たったり、探索範囲外になった場合はreturn
+#         return
+#
+#     c_x[x][y] = "#"  # 探索済みを示すためのマーキング
+#
+#     dx = [1, -1]
+#     dy = [1, -1]
+#
+#     if (1 <= x < 9) and (1 <= y < 9):
+#         if (c_x[x + dx[0]][y] == 'o' and c_x[x + dx[1]][y] == 'o') or (c_x[x][y + dy[0]] == 'o' and c_x[x][y + dy[1]] == 'o'):
+#             print('YES')
+#             sys.exit()
+#
+#     dfs_x(x + 1, y)  # これだとxがした方向、yが右方向
+#     dfs_x(x - 1, y)
+#     dfs_x(x, y + 1)
+#     dfs_x(x, y - 1)
+#
+#
+# cnt = 0
+# for i in range(10):
+#     for j in range(10):
+#         if c_o[i][j] == 'o':
+#             cnt += 1
+#             dfs_o(i, j)
+#             break
+#
+# if cnt == 1:
+#     print('YES')
+#     sys.exit()
+#
+# for i in range(10):
+#     for j in range(10):
+#         if c_x[i][j] == 'x':
+#             dfs_x(i, j)
+#             break
+#
+# print('NO')
