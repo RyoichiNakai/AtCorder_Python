64226816d6d0f1abdae911facb0cc0f18aebbc90
RyoichiNakai <elmobasketball0913@me.com>
2020-06-01 12:15:08 +0900

    new commit

--- ABC104C-AllGreen/test.py
+++ ABC104C-AllGreen/test.py
@@ -5,24 +5,31 @@ prob = []
 for i in range(1, d + 1):
     p, c = map(int, input().split())
     prob.append((i * 100, p, c))  # score, num, bonus
-
 print(prob)
 
+ans = 1000000000
 # repeatは
 for prd in product([0, 1], repeat=d):
-    print(prd)
+    cnt = 0
+    allScore = 0
+    for j, (score, num, bonus) in zip(prd, prob):
+        if j == 1:
+            cnt += num
+            allScore += score * num + bonus
 
+    if allScore < g:
+        for j, (score, num, bonus) in zip(prd, prob):
+            # allScoreを更新しつつボーナスをつけないように調整
+            # (score * num >= g - allScore)
+            # これがないとその難易度の問題の得点において目標点数に到達できない問題数だったとしても
+            # 目標点数に問題数を無視して到達することができてしまう
+            if j == 0 and (score * num >= g - allScore):
+                rest = (g - allScore) // score
+                cnt += rest
+                allScore += score * rest
+                break
 
-x = [5, 7, 2, 12]
-y = [0, 1]
-a = []
-# zipの使い方
-# forの引数が1つの場合:zipリストの同じインデックスの値を組み合わせたリストができる
-# forの引数が2つの場合:それぞれの値を参照することができる
-# forの引数が3つ以上の場合:エラー
-# 長さが別のリストの場合:一番短いリストに合わされる
+    if allScore >= g:
+        ans = min(ans, cnt)
 
-for x, y in zip(x, y):
-    a.append(x)
-    a.append(y)
-print(a)
+print(ans)
