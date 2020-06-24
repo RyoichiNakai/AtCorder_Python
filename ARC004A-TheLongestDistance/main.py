e153e12f07b70a80df15fa2c636ba8150e9e560c
RyoichiNakai <elmobasketball0913@me.com>
2020-06-24 17:40:09 +0900

    [finish] ARC004A

--- ARC004A-TheLongestDistance/main.py
+++ ARC004A-TheLongestDistance/main.py
@@ -1,24 +1,18 @@
 from itertools import combinations
 import math
 
-
-def get_distance(x1, y1, x2, y2):
-    d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
-    return d
-
-
 n = int(input())
 xy = [list(map(int, input().split())) for _ in range(n)]
-index = [i for i in range(n)]
+index = [_ for _ in range(n)]
 comb = list(combinations(index, 2))
 dis_list = []
-print(n, xy, index, comb)
 
 for c in comb:
-    x_1 = xy[c[0]][0]
-    x_2 = xy[c[1]][0]
-    y_1 = xy[c[0]][1]
-    y_2 = xy[c[1]][1]
-    dis_list.append((get_distance(x_1, y_1, x_2, y_2)))
+    x1 = xy[c[0]][0]
+    x2 = xy[c[1]][0]
+    y1 = xy[c[0]][1]
+    y2 = xy[c[1]][1]
+    d = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
+    dis_list.append(round(d, 6))
 
-print(min(dis_list))
+print(max(dis_list))
